53f00b74f88d90567af2ebda4ab7ba6c35d08840
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-11 12:04:02 +0530

    election   backend

--- admin/add_elections.php
+++ admin/add_elections.php
@@ -9,7 +9,7 @@
                 <input type="number" name="number_of_candidates" placeholder="No of Candidates" class="form-control" required />
             </div>
             <div class="form-group my-2">
-                <input type="text" onfocus="this.type='Date'" name="starting._date" placeholder="Starting Date" class="form-control" required />
+                <input type="text" onfocus="this.type='Date'" name="s_date" placeholder="Starting Date" class="form-control" required />
             </div>
             <div class="form-group my-2">
                 <input type="text" onfocus="this.type='Date'" name="ending_date" placeholder="Ending date" class="form-control" required />
@@ -25,13 +25,13 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">s.No</th>
-                    <th scope="col">Election Namec/th>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Election Name</th>
                     <th scope="col">Candidates </th>
                     <th scope="col">Starting Date</th>
                     <th scope="col">Ending Date</th>
                     <th scope="col">Status Date</th>
-                    <th scope="col">Action k/th>
+                    <th scope="col">Action </th>
                 </tr>
             </thead>
             <tbody>
@@ -41,19 +41,28 @@
                     <td>Otto</td>
                     <td>@mdo</td>
                 </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+              
             </tbody>
         </table>
     </div>
 </div>
+
+<?php
+
+if(isset($_POST['addElectionBtn']))
+{
+$election_topic = mysqli_real_escape_string ($db, $_POST['election_topic']);
+$number_of_candidates = mysqli_real_escape_string ($db, $_POST['number_of_candidates']);
+$s_date = mysqli_real_escape_string ($db, $_POST['s_date']);
+$ending_date = mysqli_real_escape_string ($db, $_POST['ending_date']);
+$inserted_by=$_SESSION['username'];
+$inserted_on =date("Y-m-d");
+
+$date1=date_create ($inserted_on);
+$date2=date_create ($s_date) ;
+$diff=date_diff( $date1, $date2);
+echo $diff->format ("%R%a days ") ;
+
+
+}
+?>
