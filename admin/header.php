a722ba35217703d78063244379f3bef79c091a81
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-11 15:25:20 +0530

    Candidates data fetch from db

--- admin/header.php
+++ admin/header.php
@@ -26,7 +26,7 @@ if($_SESSION['key'] != "AdminKey")
     <title>Admin panel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<link rel="stylesheet" href="./assets/css/login.css">
-		<link rel="stylesheet" href="./assets/css/style.css">
+		<link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body >
     
