de8b926e047d13072efacebb3a025853174f1d11
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-11 15:01:14 +0530

    candidate backend

--- admin/add_candidate.php
+++ admin/add_candidate.php
@@ -1,1 +1,140 @@
-hola
+<?php 
+    if(isset($_GET['added']))
+    {
+?>
+        <div class="alert alert-success my-3" role="alert">
+            Candidate has been added successfully.
+        </div>
+<?php 
+    }else if(isset($_GET['largeFile'])) {
+?>
+        <div class="alert alert-danger my-3" role="alert">
+            Candidate image is too large, please upload small file (you can upload any image upto 2mbs.).
+        </div>
+<?php
+    }else if(isset($_GET['invalidFile']))
+    {
+?>
+        <div class="alert alert-danger my-3" role="alert">
+            Invalid image type (Only .jpg, .png files are allowed) .
+        </div>
+<?php
+    }else if(isset($_GET['failed']))
+    {
+?>
+        <div class="alert alert-danger my-3" role="alert">
+            Image uploading failed, please try again.
+        </div>
+<?php
+    }
+
+?>
+
+
+<div class="row my-3">
+    <div class="col-4">
+        <h3>Add New Candidates</h3>
+        <form method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <select class="form-control" name="election_id" required> 
+                    <option value=""> Select Election </option>
+                    <?php 
+                        $fetchingElections = mysqli_query($db, "SELECT * FROM elections") OR die(mysqli_error($db));
+                        $isAnyElectionAdded = mysqli_num_rows($fetchingElections);
+                        if($isAnyElectionAdded > 0)
+                        {
+                            while($row = mysqli_fetch_assoc($fetchingElections))
+                            {
+                                $election_id = $row['id'];
+                                $election_name = $row['election_topic'];
+                                $allowed_candidates = $row['no_of_candidates'];
+
+                                // Now checking how many candidates are added in this election 
+                                $fetchingCandidate = mysqli_query($db, "SELECT * FROM candidate_details WHERE election_id = '". $election_id ."'") or die(mysqli_error($db));
+                                $added_candidates = mysqli_num_rows($fetchingCandidate);
+
+                                if($added_candidates < $allowed_candidates)
+                                {
+                        ?>
+                                <option value="<?php echo $election_id; ?>"><?php echo $election_name; ?></option>
+                        <?php
+                                }
+                            }
+                        }else {
+                    ?>
+                            <option value=""> Please add election first </option>
+                    <?php
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <input type="text" name="candidate_name" placeholder="Candidate Name" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="file" name="candidate_photo" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="text" name="candidate_details" placeholder="Candidate Details" class="form-control" required />
+            </div>
+            <input type="submit" value="Add Candidate" name="addCandidateBtn" class="btn btn-success" />
+        </form>
+    </div>   
+
+    <div class="col-8">
+        <h3>Candidate Details</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Photo</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Details</th>
+                    <th scope="col">Election</th>
+                    <th scope="col">Action </th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    $fetchingData = mysqli_query($db, "SELECT * FROM candidate_details") or die(mysqli_error($db)); 
+                    $isAnyCandidateAdded = mysqli_num_rows($fetchingData);
+
+                    if($isAnyCandidateAdded > 0)
+                    {
+                        $sno = 1;
+                        while($row = mysqli_fetch_assoc($fetchingData))
+                        {
+                            $election_id = $row['election_id'];
+                            $fetchingElectionName = mysqli_query($db, "SELECT * FROM elections WHERE id = '". $election_id ."'") or die(mysqli_error($db));
+                            $execFetchingElectionNameQuery = mysqli_fetch_assoc($fetchingElectionName);
+                            $election_name = $execFetchingElectionNameQuery['election_topic'];
+
+                            $candidate_photo = $row['candidate_photo'];
+
+                ?>
+                            <tr>
+                                <td><?php echo $sno++; ?></td>
+                                <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo" />    </td>
+                                <td><?php echo $row['candidate_name']; ?></td>
+                                <td><?php echo $row['candidate_details']; ?></td>
+                                <td><?php echo $election_name; ?></td>
+                                <td> 
+                                    <a href="#" class="btn btn-sm btn-warning"> Edit </a>
+                                    <a href="#" class="btn btn-sm btn-danger"> Delete </a>
+                                </td>
+                            </tr>   
+                <?php
+                        }
+                    }else {
+            ?>
+                        <tr> 
+                            <td colspan="7"> No any candidate is added yet. </td>
+                        </tr>
+            <?php
+                    }
+                ?>
+            </tbody>    
+        </table>
+    </div>
+</div>
