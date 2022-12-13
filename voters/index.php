29d0c544fac365e9874ad98ae9ec21efd1aeaa96
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-13 19:59:25 +0530

    Updated voters page

--- voters/index.php
+++ voters/index.php
@@ -3,9 +3,9 @@
     require_once("inc/navigation.php");
 ?>
 
-    <div class="row my-3">
+    <div class="row my-3 ">
         <div class="col-12">
-            <h3> Voters Panel </h3>
+            <h3 class="mx-3"> Voters Panel </h3>
 
             <?php 
                 $fetchingActiveElections = mysqli_query($db, "SELECT * FROM elections WHERE status = 'Active'") or die(mysqli_error($db));
@@ -18,16 +18,16 @@
                         $election_id = $data['id'];
                         $election_topic = $data['election_topic'];    
                 ?>
-                        <table class="table">
-                            <thead>
+                        <table class="table ">
+                            <thead >
                                 <tr>
-                                    <th colspan="4" class="bg-dark text-white"><h5> ELECTION TOPIC: <?php echo strtoupper($election_topic); ?></h5></th>
+                                    <th colspan="4" class="bg-dark text-white"><h5 class="mx-3"> ELECTION TOPIC: <?php echo strtoupper($election_topic); ?></h5></th>
                                 </tr>
-                                <tr>
-                                    <th> Photo </th>
-                                    <th> Candidate Details </th>
-                                    <th> N of Votes </th>
-                                    <th> Action </th>
+                                <tr  >
+                                    <th><p class="mx-3">Photo</p></th>
+                                    <th><p class="mx-3">Candidate Details </p></th>
+                                    <th><p class="mx-3">No of Votes</p></th>
+                                    <th><p class="mx-3">Action</p></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -45,8 +45,8 @@
 
                             ?>
                                     <tr>
-                                        <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo"> </td>
-                                        <td><?php echo "<b>" . $candidateData['candidate_name'] . "</b><br />" . $candidateData['candidate_details']; ?></td>
+                                        <td> <img src="<?php echo $candidate_photo; ?>" class="candidate_photo mx-3"> </td>
+                                        <td><?php echo  "<b>" . $candidateData['candidate_name'] . "</b><br />" . $candidateData['candidate_details']; ?> </td>
                                         <td><?php echo $totalVotes; ?></td>
                                         <td>
                                     <?php
