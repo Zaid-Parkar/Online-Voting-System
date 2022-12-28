db0b3611757e170ccfea0525befa5d151adec3ae
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-28 14:19:38 +0530

    Admin-hompegae UI update

--- admin/homepage.php
+++ admin/homepage.php
@@ -1,18 +1,18 @@
-
-<div class="row my-3 mx-2">  
+<div class="row my-3 mx-2">
     <div class="col-12">
-        <h3>Elections</h3>
-        <table class="table">
+        <h3><b><U>ELECTIONS</U> </b></h3>
+           
+        <table class="table table-bordered">
             <thead>
                 <tr>
                     <th scope="col">S.No</th>
                     <th scope="col">Election Name</th>
-                    <th scope="col"># Candidates</th>
+                    <th scope="col">Candidates Number</th>
                     <th scope="col">Starting Date</th>
                     <th scope="col">Ending Date</th>
                     <th scope="col">Status </th>
                     <th scope="col">Action </th>
-                    
+
                 </tr>
             </thead>
             <tbody>
@@ -28,31 +28,28 @@
                             $election_id = $row['id'];
                             
                 ?>
-                            <tr>
-                                <td><?php echo $sno++; ?></td>
-                                <td><?php echo $row['election_topic']; ?></td>
-                                <td><?php echo $row['no_of_candidate']; ?></td>
-                                <td><?php echo $row['s_date']; ?></td>
-                                <td><?php echo $row['ending_date']; ?></td>
-                                <td><?php echo $row['status']; ?></td>
-                                <td> 
-                                    <a href="index.php?viewResult=<?php echo $election_id; ?>" class="btn btn-sm btn-success"> View Results </a>
-                                </td>
-                            </tr>
+                <tr>
+                    <td><?php echo $sno++; ?></td>
+                    <td><?php echo $row['election_topic']; ?></td>
+                    <td><?php echo $row['no_of_candidate']; ?></td>
+                    <td><?php echo $row['s_date']; ?></td>
+                    <td><?php echo $row['ending_date']; ?></td>
+                    <td><?php echo $row['status']; ?></td>
+                    <td>
+                        <a href="index.php?viewResult=<?php echo $election_id; ?>" class="btn btn-sm btn-success"> View Results </a>
+                    </td>
+                </tr>
                 <?php
                         }
                     }else {
             ?>
-                        <tr> 
-                            <td colspan="7"> No any election is added yet. </td>
-                        </tr>
-            <?php
+                <tr>
+                    <td colspan="7"> No any election is added yet. </td>
+                </tr>
+                <?php
                     }
                 ?>
-            </tbody>    
+            </tbody>
         </table>
     </div>
 </div>
-
-
-
