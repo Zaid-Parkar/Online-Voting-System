73ce40a463c2ea0643a5ce5fbd2dd6e330cb6a50
Zaid Parkar <zaids.parkar@gmail.com>
2022-12-28 14:14:18 +0530

    Navbar update

--- admin/navigation.php
+++ admin/navigation.php
@@ -11,27 +11,25 @@
     <title>Document</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
- 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+
+  <div class=" navbar " id="navbarNav">
+    <ul class="navbar-nav ">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php?homepage=1">Home </a>
+        <a class="nav-link text-white " href="index.php?homepage=1">Home </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?addElectionPage=1">Add Election</a>
+        <a class="nav-link text-white" href="index.php?addElectionPage=1">Add Election</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="index.php?addCandidatePage=1">Add Candidate</a>
+        <a class="nav-link text-white" href="index.php?addCandidatePage=1">Add Candidate</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link " href="index.php?logoutpage">Logout</a>
+        <a class="nav-link text-white " href="index.php?logoutpage">Logout</a>
       </li>
     </ul>
   </div>
+  
 </nav>
 </body>
 </html>
